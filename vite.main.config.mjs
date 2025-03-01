0e1792d49680c20f57d66e4069a697104b923b9e
Santi <santiagoa375@gmail.com>
2025-03-01 16:20:28 -0300

    Configure Vite and update GitHub Actions workflow

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    outDir: ".vite/build", // Carpeta de salida para los archivos construidos
+    emptyOutDir: true, // Limpia la carpeta de salida antes de cada construcción
+    rollupOptions: {
+      input: "src/main.js", // Punto de entrada principal de tu aplicación
+      output: {
+        format: "cjs", // Formato CommonJS para compatibilidad con Electron
+      },
+    },
+  },
+});
